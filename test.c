8807b6023053e333f37a8a3480580a3d6bc4501a
zmzhan <mzhan@celestica.com>
2015-11-09 16:13:33 -0500

    test2

--- test.c
+++ test.c
@@ -1,8 +1,8 @@
 #include<unistd.h>
 #define TEST_MACRO "XXX"
 int main(int argc, char **argv)
-{
+{xxxx
 	printf("Hello World!xxx\n");
-	printf("this is a test ssss code\n");
+	prxxxintf("this is a test ssss code\n");
 	return 0;
 }
